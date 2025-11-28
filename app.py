d60c78a19485e1362b7fa97d4b7647985debb661
SAI CHIRAG B <169077463+08SAI@users.noreply.github.com>
2025-11-28 11:56:17 +0530

    Update Flask route and change port to 5000

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "RoutePulse service online for CIE set 103 - Flask v4"
+@app.route("/")
+def index():
+    return "RoutePulse service online for CIE set 103\n"
 
-if __name__ == '__main__':
-    # Bind to 0.0.0.0 so container can accept external connections
-    app.run(host='0.0.0.0', port=12104)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
